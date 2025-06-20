168178d4c685091f15ca61ec3512fc777f0327a1
MingoDynasty <github@mingodynasty.com>
2025-06-19 23:30:47 -0700

    Uncollapse everything in the sidebar by default.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,6 +44,10 @@ const config: Config = {
           routeBasePath: "/", // Serve the docs at the site's root
           sidebarPath: "./sidebars.ts",
           showLastUpdateTime: true,
+
+          // uncollapse everything by default
+          // I may want to change this in the future if/when I add more notes.
+          sidebarCollapsed: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/MingoDynasty/Mingo-Notes-V2/tree/main/",
